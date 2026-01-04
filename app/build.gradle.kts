b72c60e9f07c8532108ba57b49a9c2859928f658
Your Name <you@example.com>
2026-01-04 14:59:00 +0800

    update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
     defaultConfig {
         applicationId = "com.vayunmathur.games.wordmaker"
         minSdk = 30
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
